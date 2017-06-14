a0f3deda0c14c0cb34856f0a8c3e5d68ee9e1db4
Evgney Sikachev <esikachov@gmail.com>
2017-06-14 09:35:07 -0400

    Added handler for 200 code (#17)
    We can use json for client creation
    close #16

--- abc_server/flask/routes.py
+++ abc_server/flask/routes.py
@@ -4,9 +4,44 @@ from abc_server import auth
 from abc_server import config
 from abc_server.flask import abc_server
 from abc_server.flask import request
+from abc_server.flask import jsonify
 from abc_server.git import client as git_client
 from abc_server import settings
 from abc_server import utils
+
+
+@abc_server.route('/')
+def index():
+    return return_200()
+
+
+@abc_server.route('/init', methods=['POST'])
+def init():
+    user = auth.authenticate()
+    repo = user.create_repo()
+
+    with utils.YamlEditor(settings.CONFIG_PATH) as config:
+        config['repo_url'] = repo.ssh_url
+
+    git = git_client.GitClient(repo_url=repo.ssh_url)
+    git.clone()
+
+    return return_200()
+
+
+@abc_server.route('/add', methods=['POST'])
+def add():
+    with utils.YamlEditor(settings.CONFIG_PATH) as yaml:
+        yaml['watch'].extend(request.json["files"])
+
+    return return_200()
+
+
+@abc_server.route('/sync', methods=['POST'])
+def sync():
+    sync_method()
+
+    return return_200()
 
 
 def sync_method():
@@ -19,31 +54,5 @@ def sync_method():
     threading.Timer(24 * 3600, sync_method).start()
 
 
-@abc_server.route('/')
-def index():
-    return ''
-
-
-@abc_server.route('/init', methods=['POST'])
-def init():
-    user = auth.authenticate()
-    repo = user.create_repo()
-    with utils.YamlEditor(settings.CONFIG_PATH) as config:
-        config['repo_url'] = repo.ssh_url
-    git = git_client.GitClient(repo_url=repo.ssh_url)
-    git.clone()
-
-    return ''
-
-
-@abc_server.route('/add', methods=['POST'])
-def add():
-    with utils.YamlEditor(settings.CONFIG_PATH) as yaml:
-        yaml['watch'].extend(request.json["files"])
-    return ''
-
-
-@abc_server.route('/sync', methods=['POST'])
-def sync():
-    sync_method()
-    return ''
+def return_200(**kwargs):
+    return jsonify(status=200, **kwargs)
